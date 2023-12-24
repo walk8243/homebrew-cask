7ebf7a5103ac101032883701ae45c0d9ca540012
のぶ <walk8243@gmail.com>
2023-12-24 23:09:49 +0900

    Update amethyst.rb

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,6 +1,6 @@
 cask "amethyst" do
-  version "0.7.0"
-  sha256 "216c541aeb3b0c9be2734a299590f7a80c851a1578e779abe1c1fb613bcbfbc4"
+  version "0.8.0"
+  sha256 "c28cb0974b5d5d90471b3027aa0e70858af16a4b4b2de09e03e7806b32b579c0"
 
   url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-mac.zip"
   name "Amethyst"
