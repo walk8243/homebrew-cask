e40d6a72b7c6aab5bb8a92e13232b651372f2325
のぶ <walk8243@gmail.com>
2023-12-27 18:06:49 +0900

    Amethyst側からCasksを更新できるようにする (#1)
    * Create main.yml
    
    * Update main.yml
    
    * Create template amethyst.rb
    
    * Update main.yml

---
+++ template/casks/amethyst.rb
@@ -0,0 +1,11 @@
+cask "amethyst" do
+  version "CASK_VERSION"
+  sha256 "CASK_SHA256"
+
+  url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-mac.zip"
+  name "Amethyst"
+  desc "Desktop app to view and manage GitHub Issues that are relevant to me."
+  homepage "https://github.com/walk8243/amethyst-electron"
+
+  app "amethyst.app"
+end
