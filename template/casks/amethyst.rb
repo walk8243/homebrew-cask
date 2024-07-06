b3777cc2ca0563b39f5962de7b2428c301007014
のぶ <walk8243@gmail.com>
2024-07-07 00:53:45 +0900

    テンプレートを更新

--- template/casks/amethyst.rb
+++ template/casks/amethyst.rb
@@ -1,8 +1,16 @@
 cask "amethyst" do
   version "CASK_VERSION"
-  sha256 "CASK_SHA256"
 
-  url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-mac.zip"
+  on_arm do
+    arch "mac"
+    sha256 "CASK_SHA256"
+  end
+  on_intel do
+    arch "mac-intel"
+    sha256 "CASK_SHA256_INTEL"
+  end
+
+  url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-#{arch}.zip"
   name "Amethyst"
   desc "A simple issue reader for GitHub"
   homepage "https://github.com/walk8243/amethyst-electron"
