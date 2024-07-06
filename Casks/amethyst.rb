ccb7368873a68ab2555d83d58a7f102fa29e0bf3
のぶ <walk8243@gmail.com>
2024-07-07 00:41:46 +0900

    intel Macでもインストールできるようにする

--- Casks/amethyst.rb
+++ Casks/amethyst.rb
@@ -1,8 +1,15 @@
 cask "amethyst" do
+  arch arm: "mac", intel: "mac-intel"
   version "v1.15.1"
-  sha256 "528c0a8e2caf25d770114ae083a65354fd0acd3d171e9f626b852542ba54635d"
 
-  url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-mac.zip"
+  on_arm do
+    sha256 "528c0a8e2caf25d770114ae083a65354fd0acd3d171e9f626b852542ba54635d"
+  end
+  on_intel do
+    sha256 "9f462d2cbb13f6709ea44a56fff32cc847ee6e9a7ea8665c6998085ae6e943fb"
+  end
+
+  url "https://github.com/walk8243/amethyst-electron/releases/download/#{version}/amethyst-#{version}-#{arch}.zip"
   name "Amethyst"
   desc "A simple issue reader for GitHub"
   homepage "https://github.com/walk8243/amethyst-electron"
